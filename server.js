7c2b294fb889838b98ffe7e66f62900b315dae36
nard-calma <nard.calma@gmail.com>
2023-03-03 18:04:18 -0800

    added cors option *

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.options("*", cors())
 // ROUTES
+app.options('*', cors())
 const routes = require("./routes")
 app.get('/', (req, res) => {
     res.send("Spentrace Back End")
