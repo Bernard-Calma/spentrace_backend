c5a4b35c1d7234dc5624584cb9f2d9e182703cb6
Bernard Calma <nard.calma@gmail.com>
2023-03-29 01:55:24 -0700

    installed axios

--- controllers/bills.ctrls.js
+++ controllers/bills.ctrls.js
@@ -2,7 +2,7 @@ const db = require("../models")
 
 // ROUTES
 const index = (req, res) => {
-    console.log("Index route called");
+    console.log("Index route (Bills) called");
     // console.log("Plans Index Session: ", req.params.id)
     db.bills.find({userId: req.params.id}, (err, allBills) => {
         // console.log("Plans Index allPlans: ", allPlans)
