99d82729496a9e2ea422ed057d3ee9f359df02f2
Bernard Calma <nard.calma@gmail.com>
2023-04-07 02:36:06 -0700

    login route completed

--- controllers/users.ctrls.js
+++ controllers/users.ctrls.js
@@ -25,7 +25,6 @@ const loginUser = (req, res) => {
         else {
             userFound.password = undefined // Remove password when sending back user data
             req.session.currentUser = userFound; // Add user to session
-            // console.log("Login Session:", req.session)
             return (res.status(200).json(userFound))
         }
     })
