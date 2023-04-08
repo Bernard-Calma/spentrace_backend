a8fced9d4a4bd2dcce77ab52377d5b127323ecbd
Bernard Calma <nard.calma@gmail.com>
2023-04-08 14:27:07 -0700

    added await in login

--- controllers/users.ctrls.js
+++ controllers/users.ctrls.js
@@ -3,22 +3,28 @@ const bcrypt = require("bcrypt"); // To hash password
 
 // ROUTES
 // LOGIN
-const loginUser = (req, res) => {
-    console.log("Username tried to login: ", req.body.username)
-    db.Users.findOne({username: req.body.username.toLowerCase()}, (err, userFound) => {
-        if (!userFound) return res.status(404).json({message: "Username is not registered"})
-        else if (!bcrypt.compareSync(req.body.password, userFound.password)) return(res.status(401).json({message: "Invalid Username or Password"}))
-        else {
-            userFound.password = undefined // Remove password when sending back user data
-            req.session.currentUser = userFound; // Add user to session
-            console.log("Added user in session: ", req.session)
-            return (res.status(200).json({
-                user: userFound,
-                session: req.session
-            }))
-        }
-    })
-}
+const loginUser = async (req, res) => {
+    try {
+      const userFound = await db.Users.findOne({username: req.body.username.toLowerCase()})
+      if (!userFound) {
+        return res.status(404).json({message: "Username is not registered"})
+      }
+      const isMatch = await bcrypt.compare(req.body.password, userFound.password)
+      if (!isMatch) {
+        return res.status(401).json({message: "Invalid Username or Password"})
+      }
+      userFound.password = undefined
+      req.session.currentUser = userFound
+      console.log("Added user in session: ", req.session)
+      return res.status(200).json({
+        user: userFound,
+        session: req.session
+      })
+    } catch (err) {
+      console.error(err)
+      return res.status(500).json({message: "Internal Server Error"})
+    }
+  }
 
 // REGISTER
 const register = (req,res) => {
@@ -27,7 +33,7 @@ const register = (req,res) => {
     // Check username format
     const regex = /[a-zA-Z0-9]$/
     if(!regex.exec(req.body.username)) {
-        res.status(400).send({message: 'Username can only consist of letters and numbers'})
+        return res.status(400).send({message: 'Username can only consist of letters and numbers'})
     } else {
     // Check if email or username is already used been used
         db.Users.find({$or:[
@@ -35,13 +41,13 @@ const register = (req,res) => {
                 {username: newUser.username}
             ]}, (err, foundUser) => {
             if(foundUser.length > 0) {
-                console.log(foundUser)
+                // console.log(foundUser.length)
                 console.log("Username or Email is already being used")
-                res.status(400).json({message: "Username or Email is already been used."})
+                return res.status(400).json({message: "Username or Email is already been used."})
             } else {
                 // Proceed with registration
                 // Password Validation
-                // if(newUser.password.length < 6) res.status(400).json({message: "Password should be at least 6 characters"})
+                if(newUser.password.length < 6) res.status(400).json({message: "Password should be at least 6 characters"})
                 if(newUser.password !== newUser.verifyPassword) res.status(400).json({message: "Password does not match"})
                 else {
                     // Hash password
@@ -53,11 +59,11 @@ const register = (req,res) => {
                     db.Users.create(newUser, (error, createdUser) => {
                         if (error) {
                             console.log("Error creating new user")
-                            res.status(404).json({messsage: "Unexpected error occured"})
+                            return res.status(404).json({messsage: "Unexpected error occured"})
                         } else {
-                            delete createdUser.password
+                            createdUser.password = undefined
                             req.session.currentUser = createdUser
-                            res.status(200).json(createdUser)
+                            return res.status(200).json(createdUser)
                         }
                     })
                 }
