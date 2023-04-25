b2ea133b6fbcdccc8a7005db285bab576fc43ce4
Bernard Calma <nard.calma@gmail.com>
2023-04-24 20:46:29 -0700

    create account controller created

--- controllers/accounts.ctrls.js
+++ controllers/accounts.ctrls.js
@@ -1,7 +1,14 @@
 const db = require('../models')
 
 const create = (req, res) => {
-    console.log(req.body)
+    const newAccount = req.body
+    for (const [key, value] of Object.entries(newAccount)) {
+        // console.log(`${key}:${value}`)
+        if(!value) delete newAccount[key]
+        if(key === 'balance') newAccount[key] = Number(value)
+    }
+    console.log(typeof(newAccount.balance))
+    res.send(newAccount)
 }
 
 module.exports = {
